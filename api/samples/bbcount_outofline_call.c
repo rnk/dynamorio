9efad8e2d38443228df763764333e60ea5b7f23a
Reid N Kleckner <rnk@ether.csail.mit.edu>
2011-02-14 16:09:53 -0500

    Remove some things that were not meant to be committed.

--- api/samples/bbcount_outofline_call.c
+++ api/samples/bbcount_outofline_call.c
@@ -84,69 +84,10 @@ do_counting(void)
     global_bb_count++;
 }
 
-static void do_counting00(void) { global_bb_count++; }
-static void do_counting01(void) { global_bb_count++; }
-static void do_counting02(void) { global_bb_count++; }
-static void do_counting03(void) { global_bb_count++; }
-static void do_counting04(void) { global_bb_count++; }
-static void do_counting05(void) { global_bb_count++; }
-static void do_counting06(void) { global_bb_count++; }
-static void do_counting07(void) { global_bb_count++; }
-static void do_counting08(void) { global_bb_count++; }
-static void do_counting09(void) { global_bb_count++; }
-static void do_counting10(void) { global_bb_count++; }
-static void do_counting11(void) { global_bb_count++; }
-static void do_counting12(void) { global_bb_count++; }
-static void do_counting13(void) { global_bb_count++; }
-static void do_counting14(void) { global_bb_count++; }
-static void do_counting15(void) { global_bb_count++; }
-static void do_counting16(void) { global_bb_count++; }
-static void do_counting17(void) { global_bb_count++; }
-static void do_counting18(void) { global_bb_count++; }
-static void do_counting19(void) { global_bb_count++; }
-static void do_counting20(void) { global_bb_count++; }
-static void do_counting21(void) { global_bb_count++; }
-static void do_counting22(void) { global_bb_count++; }
-static void do_counting23(void) { global_bb_count++; }
-static void do_counting24(void) { global_bb_count++; }
-static void do_counting25(void) { global_bb_count++; }
-static void do_counting26(void) { global_bb_count++; }
-static void do_counting27(void) { global_bb_count++; }
-static void do_counting28(void) { global_bb_count++; }
-static void do_counting29(void) { global_bb_count++; }
-static void do_counting30(void) { global_bb_count++; }
-static void do_counting31(void) { global_bb_count++; }
-static void do_counting32(void) { global_bb_count++; }
-static void do_counting33(void) { global_bb_count++; }
-static void do_counting34(void) { global_bb_count++; }
-static void do_counting35(void) { global_bb_count++; }
-static void do_counting36(void) { global_bb_count++; }
-static void do_counting37(void) { global_bb_count++; }
-static void do_counting38(void) { global_bb_count++; }
-static void do_counting39(void) { global_bb_count++; }
-
-typedef void (*count_func)(void);
-
-count_func fptr_array[] = {
-    do_counting00, do_counting01, do_counting02, do_counting03, do_counting04,
-    do_counting05, do_counting06, do_counting07, do_counting08, do_counting09,
-    do_counting10, do_counting11, do_counting12, do_counting13, do_counting14,
-    do_counting15, do_counting16, do_counting17, do_counting18, do_counting19,
-    do_counting20, do_counting21, do_counting22, do_counting23, do_counting24,
-    do_counting25, do_counting26, do_counting27, do_counting28, do_counting29,
-    do_counting30, do_counting31, do_counting32, do_counting33, do_counting34,
-    do_counting35, do_counting36, do_counting37, do_counting38, do_counting39,
-    do_counting,
-};
-
-int fptr_counter = 0;
-
 static dr_emit_flags_t
 event_basic_block(void *drcontext, void *tag, instrlist_t *bb,
                   bool for_trace, bool translating)
 {
-    count_func f = fptr_array[fptr_counter];
-    fptr_counter = (fptr_counter + 1) % 40;
-    drcalls_shared_call(drcontext, bb, instrlist_first(bb), f, 0);
+    drcalls_shared_call(drcontext, bb, instrlist_first(bb), do_counting, 0);
     return DR_EMIT_DEFAULT;
 }
