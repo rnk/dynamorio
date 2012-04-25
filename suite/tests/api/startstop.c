672dbf51ebbb74093a07982c8259df3b597eef21
Reid Kleckner <rnk@google.com>
2012-04-25 13:54:34 -0400

    api.startstop now tests if we take over other threads.

--- suite/tests/api/startstop.c
+++ suite/tests/api/startstop.c
@@ -38,10 +38,50 @@
 #include "configure.h"
 #include "dr_api.h"
 #endif
+#include "tools.h"
+#include "threads.h"
+#ifdef WINDOWS
+# include <windows.h>
+#endif
 
 #define ITERS 150000
 
-void foo() 
+/* We have event bb look for this to make sure we're instrumenting the sideline
+ * thread.  
+ */
+NOINLINE void sideline_func(void) { }
+
+static bool took_over_sideline = false;
+
+static dr_emit_flags_t
+event_bb(void *drcontext, void *tag, instrlist_t *bb, bool for_trace,
+         bool translating)
+{
+    if (instr_get_app_pc(instrlist_first(bb)) == (app_pc)&sideline_func)
+        took_over_sideline = true;
+    return DR_EMIT_DEFAULT;
+}
+
+/* This is a thread that spins and calls sideline_func.  It will call
+ * sideline_func at least once before returning and joining the parent.
+ */
+static volatile bool should_spin = true;
+
+int
+sideline_spinner(void *unused_arg)
+{
+    do {
+        sideline_func();
+#ifdef WINDOWS
+        Sleep(0);
+#else
+        sleep(0);
+#endif
+    } while (should_spin);
+    return 0;
+}
+
+void foo(void)
 {
 }
 
@@ -49,10 +89,26 @@ int main(void)
 {
     double res = 0.;
     int i,j;
+    void *stack = NULL;
+    thread_t thread;
+    ptr_uint_t tid;
+
 #ifdef USE_DYNAMO
     dr_app_setup();
 #endif
-    
+
+    dr_register_bb_event(event_bb);
+
+    /* Create a spinning sideline thread. */
+    thread = thread_create(sideline_spinner, NULL, &stack);
+#ifdef USE_DYNAMO
+# ifdef WINDOWS
+    tid = GetThreadId(thread);
+# else /* LINUX */
+    tid = thread;
+# endif /* LINUX */
+#endif
+
     for (j=0; j<10; j++) {
 #ifdef USE_DYNAMO
         dr_app_start();
@@ -71,9 +127,17 @@ int main(void)
     }
     /* PR : we get different floating point results on different platforms,
      * so we no longer print out res */
-    printf("all done: %d iters\n", i);
-    
+    print("all done: %d iters\n", i);
+
+    if (!took_over_sideline)
+        print("failed to take over sideline thread!\n");
+
+    should_spin = false;  /* Break it's loop. */
+    delete_thread(thread, stack);
+
 #ifdef USE_DYNAMO
     dr_app_cleanup();
 #endif
+
+    return 0;
 }
