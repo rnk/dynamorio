rnk/dynamorio

commit 393219f59a473f82fe5fa95dbfc7c49476f9d912

i#787: Estimate the app's TLS size instead of using page alignment.
From the glibc source code, the page alignment approach doesn't look
reliable, and it doesn't work on some of our bots.

The downside to using size estimates is that now we have to use
safe_read() in case our estimate was wrong.

Review URL: https://codereview.appspot.com/6292043

git-svn-id: https://dynamorio.googlecode.com/svn/trunk@1383 49cc7528-f6fd-11dd-9d1a-b59b2e1864b6

core/linux/os.c
@@ -3728,7 +3728,7 @@ safe_read_ex(const void *base, size_t size, void *out_buf, size_t *bytes_read)
     dcontext_t *dcontext = get_thread_private_dcontext();
     bool res = false;
     STATS_INC(num_safe_reads);
-    if (dcontext != NULL) {
+    if (dcontext != NULL && dcontext != GLOBAL_DCONTEXT) {
         TRY_EXCEPT(dcontext, {
             memcpy(out_buf, base, size);
             res = true;
@@ -8072,9 +8072,11 @@ query_memory_ex_from_os(const byte *pc, OUT dr_mem_info_t *info)
          * and less likely to be querying a random mmapped file.
          * The cleaner fix is to allow safe_read to work w/o a dcontext: PR 529066.
          */
+        dcontext_t *dcontext = get_thread_private_dcontext();
         if (TEST(MEMPROT_READ, info->prot) &&
-            is_elf_so_header(info->base_pc, (get_thread_private_dcontext() == NULL) ? 
-                             info->size : 0))
+            is_elf_so_header(info->base_pc, ((dcontext == NULL ||
+                                              dcontext == GLOBAL_DCONTEXT) ? 
+                                             info->size : 0)))
             info->type = DR_MEMTYPE_IMAGE;
         else {
             /* FIXME: won't quite match find_executable_vm_areas marking as

core/linux/loader.c
@@ -84,7 +84,7 @@ static const char *system_lib_paths[] = {
 
 static os_privmod_data_t *libdr_opd;
 static bool privmod_initialized = false;
-static size_t max_client_tls_size = PAGE_SIZE;
+static size_t max_client_tls_size = 2 * PAGE_SIZE;
 
 #ifdef INTERNAL
 static bool printed_gdb_commands = false;
@@ -946,10 +946,32 @@ typedef struct _tls_info_t {
 } tls_info_t;
 static tls_info_t tls_info;
 
-/* The actual tcb size is the size of struct pthread from nptl/descr.h in 
- * libc source code, not a standard header file, cannot include but calculate.
+/* The actual tcb size is the size of struct pthread from nptl/descr.h, which is
+ * a glibc internal header that we can't include.  We hardcode a guess for the
+ * tcb size, and try to recover if we guessed too large.  This value was
+ * recalculated by building glibc and printing sizeof(struct pthread) from
+ * _dl_start() in elf/rtld.c.  The value can also be determined from the
+ * assembly of _dl_allocate_tls_storage() in ld.so:
+ * Dump of assembler code for function _dl_allocate_tls_storage:
+ *    0x00007ffff7def0a0 <+0>:  push   %r12
+ *    0x00007ffff7def0a2 <+2>:  mov    0x20eeb7(%rip),%rdi # _dl_tls_static_align
+ *    0x00007ffff7def0a9 <+9>:  push   %rbp
+ *    0x00007ffff7def0aa <+10>: push   %rbx
+ *    0x00007ffff7def0ab <+11>: mov    0x20ee9e(%rip),%rbx # _dl_tls_static_size
+ *    0x00007ffff7def0b2 <+18>: mov    %rbx,%rsi
+ *    0x00007ffff7def0b5 <+21>: callq  0x7ffff7ddda88 <__libc_memalign@plt>
+ * => 0x00007ffff7def0ba <+26>: test   %rax,%rax
+ *    0x00007ffff7def0bd <+29>: mov    %rax,%rbp
+ *    0x00007ffff7def0c0 <+32>: je     0x7ffff7def180 <_dl_allocate_tls_storage+224>
+ *    0x00007ffff7def0c6 <+38>: lea    -0x900(%rax,%rbx,1),%rbx
+ *    0x00007ffff7def0ce <+46>: mov    $0x900,%edx
+ * This is typically an allocation larger than 4096 bytes aligned to 64 bytes.
+ * The "lea -0x900(%rax,%rbx,1),%rbx" instruction computes the thread pointer to
+ * install.  The allocator used by the loader has no headers, so we don't have a
+ * good way to guess how big this allocation was.  Instead we use this estimate.
  */
-static size_t tcb_size;  /* thread control block size */
+static size_t tcb_size = IF_X64_ELSE(0x900, 0x490);
+
 /* thread control block header type from 
  * nptl/sysdeps/x86_64/tls.h and nptl/sysdeps/i386/tls.h 
  */
@@ -962,12 +984,18 @@ typedef struct _tcb_head_t {
     int gscope_flag;
 #endif
     ptr_uint_t sysinfo;
+    /* Later fields are copied verbatim. */
+
     ptr_uint_t stack_guard;
     ptr_uint_t pointer_guard;
 } tcb_head_t;
 
-#define TCB_TLS_ALIGN 32
-/* The size we reserved for App's libc tls. */
+/* An estimate of the size of the static TLS data before the thread pointer that
+ * we need to copy on behalf of libc.  When loading modules that have variables
+ * stored in static TLS space, the loader stores them prior to the thread
+ * pointer and lets the app intialize them.  Until we stop using the app's libc
+ * (i#46), we need to copy this data from before the thread pointer.
+ */
 #define APP_LIBC_TLS_SIZE 0x200
 
 /* FIXME: add description here to talk how TLS is setup. */
@@ -1018,7 +1046,9 @@ void *
 privload_tls_init(void *app_tp)
 {
     app_pc dr_tp;
+    tcb_head_t *dr_tcb;
     uint i;
+    size_t tls_bytes_read;
 
     if (app_tp == NULL) {
         /* FIXME: This should be a thread log, but dcontext is not ready now. */
@@ -1026,24 +1056,14 @@ privload_tls_init(void *app_tp)
         return NULL;
     }
     dr_tp = heap_mmap(max_client_tls_size);
-    LOG(GLOBAL, LOG_LOADER, 2, "%s allocates %d at "PFX"\n",
+    LOG(GLOBAL, LOG_LOADER, 2, "%s: allocates %d at "PFX"\n",
         __FUNCTION__, max_client_tls_size, dr_tp);
-    /* The current implementation of thread control block (tcb) 
-     * initialization in libc does not cross page boundary.
-     * In x86 architecture, it first allocates page-aligned memory,
-     * and then uses memory at the end of the last page for tcb, 
-     * so we assume the tcb is until the end of a page.
-     */
-    /* When app_tp is page aligned, assume tcb_size is PAGE_SIZE instead of 0,
-     * so add 1 before aligning forward.
-     */
-    tcb_size = ALIGN_FORWARD(app_tp + 1, PAGE_SIZE) - (reg_t)app_tp;
-    ASSERT(tls_info.offset <= max_client_tls_size - tcb_size);
     dr_tp = dr_tp + max_client_tls_size - tcb_size;
-    LOG(GLOBAL, LOG_LOADER, 2, "%s adjust thread pointer to "PFX"\n",
+    dr_tcb = (tcb_head_t *) dr_tp;
+    LOG(GLOBAL, LOG_LOADER, 2, "%s: adjust thread pointer to "PFX"\n",
         __FUNCTION__, dr_tp);
     /* We copy the whole tcb to avoid initializing it by ourselves. 
-     * and update some field accordingly.
+     * and update some fields accordingly.
      */
     /* DynamoRIO shares the same libc with the application, 
      * so as the tls used by libc. Thus we need duplicate
@@ -1051,13 +1071,22 @@ privload_tls_init(void *app_tp)
      * This copy can be avoided if we remove the DR's dependency on
      * libc. 
      */
-    memcpy((app_pc)ALIGN_BACKWARD(dr_tp, PAGE_SIZE),
-           (app_pc)ALIGN_BACKWARD(app_tp, PAGE_SIZE),
-           PAGE_SIZE);
-    ((tcb_head_t *)dr_tp)->tcb  = dr_tp;
-    ((tcb_head_t *)dr_tp)->self = dr_tp;
+    /* XXX: The dcontext is not installed, so the safe_read_ex always triggers a
+     * /proc/pid/maps read.
+     */
+    if (!safe_read_ex(app_tp - APP_LIBC_TLS_SIZE, APP_LIBC_TLS_SIZE + tcb_size,
+                      dr_tp  - APP_LIBC_TLS_SIZE, &tls_bytes_read)) {
+        LOG(GLOBAL, LOG_LOADER, 2, "%s: read failed, tcb was 0x%lx bytes "
+            "instead of 0x%lx\n", __FUNCTION__, tls_bytes_read -
+            APP_LIBC_TLS_SIZE, tcb_size);
+    }
+    ASSERT_CURIOSITY(tls_bytes_read == APP_LIBC_TLS_SIZE + tcb_size);
+    ASSERT(tls_info.offset <= max_client_tls_size - tcb_size);
+    /* Update two self pointers. */
+    dr_tcb->tcb  = dr_tcb;
+    dr_tcb->self = dr_tcb;
     /* i#555: replace app's vsyscall with DR's int0x80 syscall */
-    ((tcb_head_t *)dr_tp)->sysinfo = (ptr_uint_t)client_int_syscall;
+    dr_tcb->sysinfo = (ptr_uint_t)client_int_syscall;
 
     for (i = 0; i < tls_info.num_mods; i++) {
         os_privmod_data_t *opd = tls_info.mods[i]->os_privmod_data;
@@ -1082,7 +1111,7 @@ privload_tls_exit(void *dr_tp)
 {
     if (dr_tp == NULL)
         return;
-    dr_tp = (app_pc)ALIGN_FORWARD(dr_tp, PAGE_SIZE) - max_client_tls_size;
+    dr_tp = dr_tp + tcb_size - max_client_tls_size;
     heap_munmap(dr_tp, max_client_tls_size);
 }
 

commit 598b9f257e29b0fa7462a2491193b7ff9107ca50

i#350: Support safe_read without a dcontext on Linux.
This implements the Linux kernel fault handling strategy.  Each PC that
can fault has its address taken, and if we catch a fault at one of those
PCs, we return control from the signal handler to the recovery label.
The recovery label points into the normal return path, which returns the
current source pointer (XSI).

The C wrapper computes the true number of bytes copied, optionally
writes it out, and returns success or failure.

This should fix a performance regression I introduced in r1383, where
the safe_read happens before the dcontext is installed.  The read falls
through to is_readable_without_exception, which currently uses the
/proc/pid/maps parser instead of our allmem cache.  Eventually, we
should re-enable allmem (case 9745) for this query.

core/linux/os.c
@@ -3722,37 +3722,20 @@ print_all_memory_areas(file_t outf)
 }
 #endif
 
+/* C-level wrapper around the asm implementation.  Shuffles arguments and
+ * increments stats.
+ */
 bool
 safe_read_ex(const void *base, size_t size, void *out_buf, size_t *bytes_read)
 {
-    dcontext_t *dcontext = get_thread_private_dcontext();
-    bool res = false;
+    byte *stop_pc;
+    size_t nbytes;
     STATS_INC(num_safe_reads);
-    if (dcontext != NULL && dcontext != GLOBAL_DCONTEXT) {
-        TRY_EXCEPT(dcontext, {
-            memcpy(out_buf, base, size);
-            res = true;
-         } , { /* EXCEPT */
-            /* nothing: res is already false */
-         });
-    } else {
-        /* this is subject to races, but should only happen at init/attach when
-         * there should only be one live thread.
-         */
-        if (is_readable_without_exception(base, size)) {
-            memcpy(out_buf, base, size);
-            res = true;
-        }
-    }
-    if (res) {
-        if (bytes_read != NULL)
-            *bytes_read = size;
-        return true;
-    } else {
-        if (bytes_read != NULL)
-            *bytes_read = 0;
-        return false;
-    }
+    stop_pc = safe_read_asm(out_buf, base, size);
+    nbytes = stop_pc - (byte*)base;
+    if (bytes_read != NULL)
+        *bytes_read = nbytes;
+    return (nbytes == size);
 }
 
 /* FIXME - fold this together with safe_read_ex() (is a lot of places to update) */

core/linux/signal.c
@@ -3738,6 +3738,15 @@ sig_take_over(struct sigcontext *sc)
     ASSERT_NOT_REACHED();
 }
 
+static bool
+is_safe_read_pc(kernel_ucontext_t *ucxt)
+{
+    app_pc pc = (app_pc)ucxt->uc_mcontext.SC_XIP;
+    return (pc == (app_pc)safe_read_asm_pre ||
+            pc == (app_pc)safe_read_asm_mid ||
+            pc == (app_pc)safe_read_asm_post);
+}
+
 /* the master signal handler
  * WARNING: behavior varies with different versions of the kernel!
  * sigaction support was only added with 2.2
@@ -3774,6 +3783,16 @@ master_signal_handler(int sig, siginfo_t *siginfo, kernel_ucontext_t *ucxt)
 #endif
     bool local;
     dcontext_t *dcontext = get_thread_private_dcontext();
+
+    /* i#350: To support safe_read without a dcontext, use the global dcontext
+     * when handling safe_read faults.  This lets us pass the check for a
+     * dcontext below and causes us to use the global log.
+     */
+    if (dcontext == NULL && (sig == SIGSEGV || sig == SIGBUS) &&
+        is_safe_read_pc(ucxt)) {
+        dcontext = GLOBAL_DCONTEXT;
+    }
+
     if (dynamo_exited && get_num_threads() > 1 && sig == SIGSEGV) {
         /* PR 470957: this is almost certainly a race so just squelch it.
          * We live w/ the risk that it was holding a lock our release-build
@@ -3825,9 +3844,13 @@ master_signal_handler(int sig, siginfo_t *siginfo, kernel_ucontext_t *ucxt)
      * hang if signal interrupts DR: but we don't really support that option
      */
     ENTERING_DR();
-    local = local_heap_protected(dcontext);
-    if (local)
-        SELF_PROTECT_LOCAL(dcontext, WRITABLE);
+    if (dcontext == GLOBAL_DCONTEXT) {
+        local = false;
+    } else {
+        local = local_heap_protected(dcontext);
+        if (local)
+            SELF_PROTECT_LOCAL(dcontext, WRITABLE);
+    }
 
     LOG(THREAD, LOG_ASYNCH, level, "\nmaster_signal_handler: sig=%d, retaddr="PFX"\n",
         sig, *((byte **)xsp));
@@ -3898,7 +3921,7 @@ master_signal_handler(int sig, siginfo_t *siginfo, kernel_ucontext_t *ucxt)
             ASSERT_NOT_REACHED();
         }
 #endif
-        if (dcontext->try_except_state != NULL) {
+        if (is_safe_read_pc(ucxt) || dcontext->try_except_state != NULL) {
             /* handle our own TRY/EXCEPT */
 #ifdef HAVE_PROC_MAPS
             /* our probe produces many of these every run */
@@ -3908,6 +3931,13 @@ master_signal_handler(int sig, siginfo_t *siginfo, kernel_ucontext_t *ucxt)
             LOG(THREAD, LOG_ALL, level, "TRY fault at "PFX"\n", pc);
             if (TEST(DUMPCORE_TRY_EXCEPT, DYNAMO_OPTION(dumpcore_mask)))
                 os_dump_core("try/except fault");
+
+            if (is_safe_read_pc(ucxt)) {
+                ucxt->uc_mcontext.SC_XIP = (reg_t)&safe_read_asm_recover;
+                /* Break out to log the normal return from the signal handler.
+                 */
+                break;
+            }
 
             /* The exception interception code did an ENTER so we must EXIT here */
             EXITING_DR();

core/x86/arch_exports.h
@@ -1864,6 +1864,21 @@ dr_longjmp(dr_jmp_buf_t *buf, int val);
 int 
 dr_setjmp(dr_jmp_buf_t *buf);
 
+#ifdef LINUX
+/* i#350: Fast safe_read without dcontext.  On success or failure, returns the
+ * current source pointer.
+ */
+void *safe_read_asm(void *dst, const void *src, size_t size);
+
+/* These are labels, not function pointers.  We declare them as functions to
+ * prevent loads and stores to these globals from compiling.
+ */
+void safe_read_asm_pre(void);
+void safe_read_asm_mid(void);
+void safe_read_asm_post(void);
+void safe_read_asm_recover(void);
+#endif /* LINUX */
+
 #define DR_SETJMP(buf) (dr_setjmp(buf))
 
 #define DR_LONGJMP(buf, val)          \
@@ -1913,6 +1928,5 @@ dr_setjmp(dr_jmp_buf_t *buf);
 /* only takes integer literals */
 # define APP_PARAM(mc, offs) (*(((reg_t *)((mc)->xsp)) + (offs) + 1))
 #endif
-
 
 #endif /* _ARCH_EXPORTS_H_ */

core/x86/x86.asm
@@ -1896,6 +1896,105 @@ GLOBAL_LABEL(hashlookup_null_handler:)
 #endif
         END_FUNC(hashlookup_null_handler)
 
+#ifdef LINUX
+
+# ifdef X64
+#  define PTRSZ_SHIFT_BITS 3
+#  define PTRSZ_SUFFIXED(string_op) string_op##q
+#  define ARGS_TO_XDI_XSI_XDX()         /* ABI handles this. */
+#  define RESTORE_XDI_XSI()             /* Not needed. */
+# else
+#  define PTRSZ_SHIFT_BITS 2
+#  define PTRSZ_SUFFIXED(string_op) string_op##d
+
+/* Get args 1, 2, 3 into edi, esi, and edx to match Linux x64 ABI.  Need to save
+ * edi and esi since they are callee-saved.  The ARGN macros can't handle
+ * stack adjustments, so use the scratch regs eax and ecx to hold the args
+ * before the pushes.
+ */
+#  define ARGS_TO_XDI_XSI_XDX() \
+        mov     eax, ARG1                       @N@\
+        mov     ecx, ARG2                       @N@\
+        mov     edx, ARG3                       @N@\
+        push    edi                             @N@\
+        push    esi                             @N@\
+        mov     edi, eax                        @N@\
+        mov     esi, ecx
+#  define RESTORE_XDI_XSI() \
+        pop esi                                 @N@\
+        pop edi
+# endif
+
+/* Repeats string_op for XDX bytes using aligned pointer-sized operations when
+ * possible.  Assumes that string_op works by counting down until XCX reaches
+ * zero.  The pointer-sized string ops are aligned based on ptr_to_align.
+ * For string ops that have both a src and dst, aligning based on src is
+ * preferred, subject to micro-architectural differences.
+ *
+ * XXX: glibc memcpy uses SSE instructions to copy, which is 10% faster on x64
+ * and ~2x faster for 20kb copies on plain x86.  Using SSE is quite complicated,
+ * because it means doing cpuid checks and loop unrolling.  Many of our string
+ * operations are short anyway.  For safe_read, it also increases the number of
+ * potentially faulting PCs.
+ */
+# define REP_STRING_OP(funcname, ptr_to_align, string_op) \
+        mov     REG_XCX, ptr_to_align                           @N@\
+        and     REG_XCX, (ARG_SZ - 1)                           @N@\
+        jz      funcname##_aligned                              @N@\
+        neg     REG_XCX                                         @N@\
+        add     REG_XCX, ARG_SZ                                 @N@\
+        cmp     REG_XDX, REG_XCX  /* if (n < xcx) */            @N@\
+        cmovb   REG_XCX, REG_XDX  /*     xcx = n; */            @N@\
+        sub     REG_XDX, REG_XCX                                @N@\
+GLOBAL_LABEL(funcname##_pre:)                                   @N@\
+        rep string_op##b                                        @N@\
+GLOBAL_LABEL(funcname##_aligned:)                               @N@\
+        /* Aligned word-size ops. */                            @N@\
+        mov     REG_XCX, REG_XDX                                @N@\
+        shr     REG_XCX, PTRSZ_SHIFT_BITS                       @N@\
+GLOBAL_LABEL(funcname##_mid:)                                   @N@\
+        rep PTRSZ_SUFFIXED(string_op)                           @N@\
+        /* Handle trailing bytes. */                            @N@\
+        mov     REG_XCX, REG_XDX                                @N@\
+        and     REG_XCX, (ARG_SZ - 1)                           @N@\
+GLOBAL_LABEL(funcname##_post:)                                  @N@\
+        rep string_op##b
+
+/* Declare these labels global so we can take their addresses in C.  pre, mid,
+ * and post are defined by REP_STRING_OP().
+ */
+DECLARE_GLOBAL(safe_read_asm_pre)
+DECLARE_GLOBAL(safe_read_asm_mid)
+DECLARE_GLOBAL(safe_read_asm_post)
+DECLARE_GLOBAL(safe_read_asm_recover)
+
+/* i#350: We implement safe_read in assembly and save the PCs that can fault.
+ * If these PCs fault, we return from the signal handler to the epilog, which
+ * can recover.  We return the source pointer from XSI, and the caller uses this
+ * to determine how many bytes were copied and whether it matches size.
+ *
+ * XXX: Use this on Windows for a perf boost over nt_read_virtual_memory().
+ *
+ * XXX: Do we care about differentiating whether the read or write faulted?
+ * Currently this is just "safe_memcpy", and we recover regardless of whether
+ * the read or write faulted.
+ *
+ * void *
+ * safe_read_asm(void *dst, const void *src, size_t n);
+ */
+        DECLARE_FUNC(safe_read_asm)
+GLOBAL_LABEL(safe_read_asm:)
+        ARGS_TO_XDI_XSI_XDX()           /* dst=xdi, src=xsi, n=xdx */
+        /* Copy xdx bytes, align on src. */
+        REP_STRING_OP(safe_read_asm, REG_XSI, movs)
+GLOBAL_LABEL(safe_read_asm_recover:)
+        mov     REG_XAX, REG_XSI        /* Return cur_src */
+        RESTORE_XDI_XSI()
+        ret
+        END_FUNC(safe_read_asm)
+
+#endif /* LINUX */
+
 /*#############################################################################
  *#############################################################################
  */

suite/tests/client-interface/file_io.dll.c
@@ -116,7 +116,7 @@ void dr_init(client_id_t id)
     byte *base_pc;
     size_t size;
     size_t bytes_read, bytes_written;
-    byte *edge, *wbuf;
+    byte *edge, *mbuf;
     bool ok;
 
     /* The Makefile will pass a full absolute path (for Windows and Linux) as the client
@@ -210,13 +210,21 @@ void dr_init(client_id_t id)
         dr_fprintf(STDERR, "ERROR in plain dr_safe_read()\n");
     }
     memset(safe_buf, 0xcd, sizeof(safe_buf));
-    edge = find_prot_edge(read_only_buf, DR_MEMPROT_READ);
+    /* read_only_buf will be in .rodata on Linux, and can be followed by string
+     * constants with the same page protections.  In order to be sure that we're
+     * copying zeroes, we map our own memory.
+     */
+    mbuf = dr_nonheap_alloc(PAGE_SIZE*3, DR_MEMPROT_READ|DR_MEMPROT_WRITE);
+    memset(mbuf, 0, PAGE_SIZE*3);
+    dr_memory_protect(mbuf + PAGE_SIZE*2, PAGE_SIZE, DR_MEMPROT_NONE);
+    edge = find_prot_edge(mbuf, DR_MEMPROT_NONE);
     bytes_read = 0xcdcdcdcd;
     if (dr_safe_read(edge - (PAGE_SIZE + 10), PAGE_SIZE+20, safe_buf, &bytes_read) ||
         bytes_read == 0xcdcdcdcd || bytes_read > PAGE_SIZE+10 || 
         !memchk(safe_buf, 0, bytes_read)) {
         dr_fprintf(STDERR, "ERROR in overlap dr_safe_read()\n");
     }
+    dr_nonheap_free(mbuf, PAGE_SIZE*3);
     dr_fprintf(STDERR, "dr_safe_read() check\n");
 
     /* test DR_TRY_EXCEPT */
@@ -237,19 +245,19 @@ void dr_init(client_id_t id)
         dr_fprintf(STDERR, "ERROR in plain dr_safe_write()\n");
     }
     /* so we don't clobber other global vars we use an allocated buffer here */
-    wbuf = dr_nonheap_alloc(PAGE_SIZE*3, DR_MEMPROT_READ|DR_MEMPROT_WRITE);
-    if (!dr_memory_protect(wbuf + PAGE_SIZE*2, PAGE_SIZE, DR_MEMPROT_READ))
+    mbuf = dr_nonheap_alloc(PAGE_SIZE*3, DR_MEMPROT_READ|DR_MEMPROT_WRITE);
+    if (!dr_memory_protect(mbuf + PAGE_SIZE*2, PAGE_SIZE, DR_MEMPROT_READ))
         dr_fprintf(STDERR, "ERROR in dr_memory_protect\n");
-    memset(wbuf, 0, sizeof(wbuf));
-    edge = find_prot_edge(wbuf, DR_MEMPROT_WRITE);
+    memset(mbuf, 0, PAGE_SIZE*2);
+    edge = find_prot_edge(mbuf, DR_MEMPROT_WRITE);
     bytes_written = 0xcdcdcdcd;
     if (dr_safe_write(edge - (PAGE_SIZE + 10), PAGE_SIZE+20, safe_buf, &bytes_written) ||
         bytes_written == 0xcdcdcdcd || bytes_written > PAGE_SIZE+10 ||
         !memchk(edge - (PAGE_SIZE + 10), 0xcd, bytes_written)) {
         dr_fprintf(STDERR, "ERROR in overlap dr_safe_write() "PFX" "PFX" %d\n",
-                   wbuf, edge, bytes_written);
+                   mbuf, edge, bytes_written);
     }
-    dr_nonheap_free(wbuf, PAGE_SIZE*3);
+    dr_nonheap_free(mbuf, PAGE_SIZE*3);
     dr_fprintf(STDERR, "dr_safe_write() check\n");
 }
 