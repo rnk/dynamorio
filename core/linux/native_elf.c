c0cffb5689f340e1b97559c57d0edf65fd323329
Reid Kleckner <rnk@google.com>
2013-02-12 14:48:46 -0500

    Add comments as suggested in review.

--- core/linux/native_elf.c
+++ core/linux/native_elf.c
@@ -87,10 +87,6 @@ static uint plt_stub_jmp_tgt_offset;
 static size_t plt_stub_size;
 static app_pc stub_heap;
 
-static app_pc
-create_plt_stub(app_pc plt_target);
-
-
 static bool
 module_contains_pc(module_area_t *ma, app_pc pc)
 {
@@ -210,6 +206,36 @@ replace_module_resolver(module_area_t *ma, app_pc *pltgot)
     }
 }
 
+/* Allocates and initializes a stub of code for taking control after a PLT call.
+ */
+static app_pc
+create_plt_stub(app_pc plt_target)
+{
+    app_pc stub_pc = special_heap_alloc(stub_heap);
+    app_pc *tgt_immed;
+    app_pc jmp_tgt;
+
+    memcpy(stub_pc, plt_stub_template, plt_stub_size);
+    tgt_immed = (app_pc *) (stub_pc + plt_stub_immed_offset);
+    jmp_tgt = stub_pc + plt_stub_jmp_tgt_offset;
+    *tgt_immed = plt_target;
+    insert_relative_target(jmp_tgt, (app_pc) native_plt_call,
+                           false/*!hotpatch*/);
+    return stub_pc;
+}
+
+/* Deletes a PLT stub and returns the original target of the stub.
+ */
+static app_pc
+destroy_plt_stub(app_pc stub_pc)
+{
+    app_pc orig_tgt;
+    app_pc *tgt_immed = (app_pc *) (stub_pc + plt_stub_immed_offset);
+    orig_tgt = *tgt_immed;
+    special_heap_free(stub_heap, stub_pc);
+    return orig_tgt;
+}
+
 static size_t
 plt_reloc_entry_size(ELF_WORD pltrel)
 {
@@ -222,26 +248,6 @@ plt_reloc_entry_size(ELF_WORD pltrel)
         ASSERT(false);
     }
     return sizeof(ELF_REL_TYPE);
-}
-
-static void
-unhook_stub(app_pc *r_addr, app_pc stub_pc)
-{
-    /* Get the original target out of the instr.  The immediate operand of the
-     * first instruction in the stub has it.  We let the accessors assert that
-     * this is so.
-     */
-    dcontext_t *dcontext = get_thread_private_dcontext();
-    instr_t instr;
-    app_pc orig_target;
-    if (dcontext == NULL)  /* happens during exit */
-        dcontext = GLOBAL_DCONTEXT;
-    instr_init(dcontext, &instr);
-    decode(dcontext, stub_pc, &instr);
-    orig_target = (app_pc) opnd_get_immed_int(instr_get_src(&instr, 0));
-    *r_addr = orig_target;
-    special_heap_free(stub_heap, stub_pc);
-    instr_free(dcontext, &instr);
 }
 
 static bool
@@ -263,8 +269,11 @@ is_special_stub(app_pc stub_pc)
     return found;
 }
 
+/* Iterates all PLT relocations and either inserts or removes our own PLT
+ * takeover stubs.
+ */
 static void
-scan_plt_relocs(module_area_t *ma, os_privmod_data_t *opd, bool add_hooks)
+update_plt_relocations(module_area_t *ma, os_privmod_data_t *opd, bool add_hooks)
 {
     app_pc jmprel;
     app_pc jmprelend = opd->jmprel + opd->pltrelsz;
@@ -277,11 +286,12 @@ scan_plt_relocs(module_area_t *ma, os_privmod_data_t *opd, bool add_hooks)
         ASSERT(module_contains_pc(ma, (app_pc)r_addr));
         gotval = *r_addr;
         if (add_hooks) {
+            /* If the PLT target is inside the current module, then it is either
+             * a lazy resolution stub or was resolved to the current module.
+             * Either way we ignore it.
+             */
             if (!module_contains_pc(ma, gotval)) {
                 LOG(THREAD_GET, LOG_LOADER, 4,
-                    "%s: hooking cross-module PLT entry to "PFX"\n",
-                    __FUNCTION__, gotval);
-                print_file(STDERR,
                     "%s: hooking cross-module PLT entry to "PFX"\n",
                     __FUNCTION__, gotval);
                 *r_addr = create_plt_stub(gotval);
@@ -291,7 +301,7 @@ scan_plt_relocs(module_area_t *ma, os_privmod_data_t *opd, bool add_hooks)
              * acquisitions.
              */
             if (is_special_stub(gotval)) {
-                unhook_stub(r_addr, gotval);
+                *r_addr = destroy_plt_stub(gotval);
             }
         }
     }
@@ -341,15 +351,13 @@ module_change_hooks(module_area_t *ma, bool add_hooks, bool at_map)
 
     if (add_hooks) {
         replace_module_resolver(ma, pltgot);
-        scan_plt_relocs(ma, &opd, add_hooks);
-    }
-    else {
-        /* Remove our hooks, but only if it looks like the module is
-         * currently hooked.
-         */
+    } else {
+        ASSERT(app_dl_runtime_resolve != NULL);
         pltgot[DL_RUNTIME_RESOLVE_IDX] = app_dl_runtime_resolve;
-        scan_plt_relocs(ma, &opd, add_hooks);
     }
+
+    /* Insert or remove our PLT stubs. */
+    update_plt_relocations(ma, &opd, add_hooks);
 
     if (got_unprotected) {
         /* XXX: This may not be symmetric, but we trust PT_GNU_RELRO for now. */
@@ -370,22 +378,6 @@ void
 native_module_unhook(module_area_t *ma)
 {
     module_change_hooks(ma, false/*remove*/, false/*!at_map*/);
-}
-
-static app_pc
-create_plt_stub(app_pc plt_target)
-{
-    app_pc stub_pc = special_heap_alloc(stub_heap);
-    app_pc *tgt_immed;
-    app_pc jmp_tgt;
-
-    memcpy(stub_pc, plt_stub_template, plt_stub_size);
-    tgt_immed = (app_pc *) (stub_pc + plt_stub_immed_offset);
-    jmp_tgt = stub_pc + plt_stub_jmp_tgt_offset;
-    *tgt_immed = plt_target;
-    insert_relative_target(jmp_tgt, (app_pc) native_plt_call,
-                           false/*!hotpatch*/);
-    return stub_pc;
 }
 
 static ELF_REL_TYPE *
@@ -466,7 +458,7 @@ void
 native_module_exit(void)
 {
     /* Make sure we can scan all modules on native_exec_areas and unhook them.
-     * If this fails, we get special heak leak asserts.
+     * If this fails, we get special heap leak asserts.
      */
     module_iterator_t *mi;
     module_area_t *ma;
