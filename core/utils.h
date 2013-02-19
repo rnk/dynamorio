5c8b80d4314deacdaa19d44c943b8bb514b9a886
Reid Kleckner <rnk@google.com>
2013-02-19 10:04:07 -0500

    Try to get the ordering right.

--- core/utils.h
+++ core/utils.h
@@ -381,12 +381,6 @@ enum {
 #endif
     LOCK_RANK(coarse_info_lock), /* < special_heap_lock, < global_alloc_lock,
                                   * > change_linking_lock */
-    LOCK_RANK(module_data_lock),  /* < loaded_module_areas, < special_heap_lock */
-    LOCK_RANK(special_units_list_lock), /* < special_heap_lock */
-    LOCK_RANK(special_heap_lock), /* > bb_building_lock, > hotp_vul_table_lock
-                                   * < dynamo_areas, < heap_unit_lock */
-    LOCK_RANK(coarse_info_incoming_lock), /* > special_heap_lock, > coarse_info_lock,
-                                           * > change_linking_lock */
 
     /* (We don't technically need a coarse_table_rwlock separate from table_rwlock
      * anymore but having it gives us flexibility so I'm leaving it)
@@ -401,6 +395,12 @@ enum {
     LOCK_RANK(executable_areas), /* < dynamo_areas < global_alloc_lock
                                   * < process_module_vector_lock (diagnostics)
                                   */
+    LOCK_RANK(module_data_lock),  /* < loaded_module_areas, < special_heap_lock */
+    LOCK_RANK(special_units_list_lock), /* < special_heap_lock */
+    LOCK_RANK(special_heap_lock), /* > bb_building_lock, > hotp_vul_table_lock
+                                   * < dynamo_areas, < heap_unit_lock */
+    LOCK_RANK(coarse_info_incoming_lock), /* > special_heap_lock, > coarse_info_lock,
+                                           * > change_linking_lock */
 #ifdef RCT_IND_BRANCH
     LOCK_RANK(rct_module_lock), /* > coarse_info_lock, > executable_areas,
                                  * < heap allocation */
