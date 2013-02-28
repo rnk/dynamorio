31caa22440d3e587b3daeec79b1a8e23884edb10
Reid Kleckner <rnk@google.com>
2013-02-28 15:30:16 -0500

    Use a stack allocated buffer for private_instr_encode
    Profiling shows that heap allocation is hot and is frequently called for
    private encoding.

--- core/x86/instr.c
+++ core/x86/instr.c
@@ -1882,8 +1882,7 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
     /* we cannot use a stack buffer for encoding since our stack on x64 linux
      * can be too far to reach from our heap
      */
-    byte *buf = heap_alloc(dcontext, 32 /* max instr length is 17 bytes */
-                           HEAPACCT(ACCT_IR));
+    byte buf[DR_INSTR_ENCODE_MAX_SIZE];
     uint len;
     /* Do not cache instr opnds as they are pc-relative to final encoding location.
      * Rather than us walking all of the operands separately here, we have
@@ -1893,11 +1892,11 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
     bool has_instr_opnds;
     byte *nxt = instr_encode_check_reachability(dcontext, instr, buf, &has_instr_opnds);
     bool valid_to_cache = !has_instr_opnds;
+    /* We only allocate buf on the heap if we can cache the encoding. */
     if (nxt == NULL) {
         nxt = instr_encode_ignore_reachability(dcontext, instr, buf);
         if (nxt == NULL) {
             SYSLOG_INTERNAL_WARNING("cannot encode %s\n", op_instr[instr->opcode]->name);
-            heap_free(dcontext, buf, 32 HEAPACCT(ACCT_IR));
             return 0;
         }
         /* if unreachable, we can't cache, since re-relativization won't work */
@@ -1906,7 +1905,8 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
     len = (int) (nxt - buf);    
     CLIENT_ASSERT(len > 0 || instr_is_label(instr),
                   "encode instr for length/eflags error: zero length");
-    CLIENT_ASSERT(len < 32, "encode instr for length/eflags error: instr too long");
+    CLIENT_ASSERT(len < DR_INSTR_ENCODE_MAX_SIZE,
+                  "encode instr for length/eflags error: instr too long");
     ASSERT_CURIOSITY(len >= 0 && len < 18);
 
     /* do not cache encoding if mangle is false, that way we can have
@@ -1937,7 +1937,7 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
          * using rip-rel-calculating routines that also use instr->bytes.
          */
         tmp = instr->bytes;
-        instr->bytes = buf;
+        instr->bytes = buf;  /* does not escape, we copy into tmp */
 #ifdef X64
         instr_set_rip_rel_valid(instr, rip_rel_valid);
 #endif
@@ -1945,7 +1945,6 @@ private_instr_encode(dcontext_t *dcontext, instr_t *instr, bool always_cache)
         instr->bytes = tmp;
         instr_set_operands_valid(instr, valid);
     }
-    heap_free(dcontext, buf, 32 HEAPACCT(ACCT_IR));
     return len;
 }
 
